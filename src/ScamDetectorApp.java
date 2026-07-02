88c5b9f9665ced237fc161732a25b637bda1300d
Gustavo Henrique Becker <gustavohenrique.becker0@gmail.com>
2026-07-02 14:28:32 -0300

    Creating ScamDetectorApp.java and inputing simple message data.

---
+++ src/ScamDetectorApp.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ScamDetectorApp {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Type your message: ");
+        String message = sc.nextLine();
+
+        System.out.println(message);
+    }
+}
+
+
