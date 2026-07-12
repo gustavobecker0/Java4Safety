3bc1ea7223a38c118bedf66fc9f9224454f281d8
Gustavo Henrique Becker <gustavohenrique.becker0@gmail.com>
2026-07-11 22:04:56 -0300

    Feat: implementing new rules architecture.

--- src/app/ScamDetector.java
+++ src/app/ScamDetector.java
@@ -1,81 +1,31 @@
     package app;
 
-public class ScamDetector {
+    import app.rules.*;
 
-    private static final String[] URGENCY_WORDS = {
-        "urgente", "imediato", "última chance", "não perca", "responda agora", "tempo limitado"
-    };
-    private static final String[] SUSPICIOUS_WORDS = {
-            "link", "clique", "aqui", "resgate", "resgatar"
-    };
+    import java.util.List;
 
-    private static final String[] MONEY_WORDS = {
-            "pix", "pagamento", "depósito", "transferência",
-            "boleto", "dinheiro", "taxa", "multa", "prêmio"
-    };
+    public class ScamDetector {
 
-    private static final String[] PRIZE_WORDS = {
-            "você ganhou", "parabéns",
-            "prêmio", "sorteio", "recompensa"
-    };
+    private final List<DetectionRule> rules = List.of(
+            new UrgencyRule(),
+            new SuspiciousRule(),
+            new MoneyRule(),
+            new PrizeRule(),
+            new PersonalDataRule(),
+            new ThreatRule()
+    );
 
-    private static final String[] PERSONAL_DATA_WORDS = {
-            "senha", "cpf", "cartão", "código",
-            "token", "cvv", "confirme seus dados"
-    };
-
-    private static final String[] THREAT_WORDS = {
-            "conta bloqueada",
-            "será bloqueado",
-            "último aviso",
-            "ação judicial",
-            "cancelado"
-    };
 
     public boolean isSuspicious(String message){
-        String lowerCaseMessage = message.toLowerCase();
+        message = message.toLowerCase();
 
-        return containsUrgency(lowerCaseMessage)
-                || containSuspiciousLink(lowerCaseMessage)
-                || containsMoneyWords(lowerCaseMessage)
-                || containsPrizeWords(lowerCaseMessage)
-                || asksForPersonalData(lowerCaseMessage)
-                || containsThreats(lowerCaseMessage)
-                || containsUrl(lowerCaseMessage);
-    }
-
-    private boolean containsAny(String message, String[] words) {
-        for (String word : words) {
-            if (message.contains(word)) {
+        for (DetectionRule rule : this.rules) {
+            if (rule.detectAny(message)) {
                 return true;
             }
         }
 
         return false;
-    }
-
-    public boolean containsUrgency(String message) {
-        return containsAny(message, URGENCY_WORDS);
-    }
-
-    public boolean containSuspiciousLink(String message) {
-        return containsAny(message, SUSPICIOUS_WORDS);
-    }
-
-    public boolean containsMoneyWords(String message) {
-        return containsAny(message, MONEY_WORDS);
-    }
-
-    public boolean containsPrizeWords(String message) {
-        return containsAny(message, PRIZE_WORDS);
-    }
-
-    public boolean asksForPersonalData(String message) {
-        return containsAny(message, PERSONAL_DATA_WORDS);
-    }
-
-    public boolean containsThreats(String message) {
-        return containsAny(message, THREAT_WORDS);
     }
 
     public boolean containsUrl(String message) {
