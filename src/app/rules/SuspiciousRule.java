ca36b63d042119ee7b7c084abddd3733d42ab78e
Gustavo Henrique Becker <gustavohenrique.becker0@gmail.com>
2026-07-11 21:55:11 -0300

    New: new class SuspicousRule that extends from abstract KeywordRule.

---
+++ src/app/rules/SuspiciousRule.java
@@ -0,0 +1,16 @@
+package app.rules;
+
+public class SuspiciousRule extends KeywordRule{
+
+    @Override
+    protected String[] keywords(){
+        return new String[]{
+                "link",
+                "clique",
+                "aqui",
+                "resgate",
+                "resgatar"
+        };
+    }
+
+}
