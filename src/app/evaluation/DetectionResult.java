aafb7e31c0f7ec75ba746968494894677eb8ef15
Gustavo Henrique Becker <gustavohenrique.becker0@gmail.com>
2026-07-13 14:20:43 -0300

    Update DetectionRule: updating method name.

---
+++ src/app/evaluation/DetectionResult.java
@@ -0,0 +1,31 @@
+package app.evaluation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DetectionResult {
+
+    private final List<Evidence> evidences = new ArrayList<>();
+
+    public void add(Evidence evidence) {
+        evidences.add(evidence);
+    }
+
+    public int getScore() {
+        int score = 0;
+
+        for (Evidence evidence : evidences) {
+            score += evidence.getWeight();
+        }
+
+        return score;
+    }
+
+    public List<Evidence> getEvidences() {
+        return evidences;
+    }
+
+    public boolean isSuspicious() {
+        return getScore() >= 5;
+    }
+}
